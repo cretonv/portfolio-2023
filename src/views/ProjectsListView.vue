4c3328741c85f1cfa00d146ee50503c276e90c1b
Vincent <vincecreton@hotmail.fr>
2023-08-11 11:56:28 +0200

    FEATURE: Add title to projects list page

--- src/views/ProjectsListView.vue
+++ src/views/ProjectsListView.vue
@@ -2,10 +2,12 @@
 import ProjectListItem from "~/src/components/ProjectListItem.vue";
 
 const { client } = usePrismic()
+const {data: general } = await useAsyncData('general', () => client.getSingle('general'))
 const { data: projects } = await useAsyncData('projects', () => client.getAllByType('project'))
 </script>
 <template>
-  <div class="relative bg-black min-h-screen h-full w-screen px-10">
+  <div class="relative bg-black min-h-screen h-full w-screen p-10">
+    <h1 class="absolute left-0 top-0 font-title text-white text-4xl pt-12 pl-10">{{general.data.projects_listing_title[0].text}}</h1>
     <div class="flex flex-wrap w-full text-white font-lato" v-if="projects">
       <ProjectListItem :key="project.id" v-for="project in projects" :project="project" />
     </div>
