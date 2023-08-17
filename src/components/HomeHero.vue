945332c430646d50a5c523f34d48c518b6d22b29
Vincent <vincecreton@hotmail.fr>
2023-08-17 14:01:32 +0200

    FEATURE: View for home and here component with code made for waiting page

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TitleH1 from "../components/TitleH1.vue";
+import LogoInitial from "../components/LogoInitial.vue";
+import Name from "../components/Name.vue";
+import BrokenCircle from "../components/BrokenCircle.vue";
+import {ref} from "vue";
+
+const container = ref(null)
+  </script>
+<template>
+  <div class="pl-10 pr-14 -md:absolute -md:pl-4 -md:pr-0"><LogoInitial class="mt-8" /></div>
+  <div class="flex flex-col justify-center -md:pl-12 -md:pr-12">
+    <TitleH1 class="mb-14">Développeur <br> FRONT-END <br> créatif</TitleH1>
+  </div>
+  <BrokenCircle class="absolute top-[20%] right-[7.5%]" :container="container" />
+  <Name class="absolute bottom-6 right-4"> vincent creton </Name>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <HomeView />
+  </div>
+</template>
