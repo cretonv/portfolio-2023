b370f57311bace717a5b766dfd3f9c76a64d51fa
Vincent <vincecreton@hotmail.fr>
2023-08-18 22:35:38 +0200

    FEATURE: Button link to projects page on home (temporary

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import DefaultButton from "~/src/components/DefaultButton.vue";
 defineProps<{
   data: object
 }>()
+const goToProjectsPage = async () => {
+  await navigateTo({ name: 'projects-list' })
+}
 </script>
 <template>
   <div class="flex px-10 items-start">
     <div v-if="data.primary" class="w-2/3 flex flex-wrap justify-center text-white">
       <h2 class="w-full text-4xl font-title">{{data.primary.title[0].text}}</h2>
-      <div class="w-3/4 text-xl mt-8 font-lato">{{data.primary.text[0].text}}</div>
+      <div class="w-3/4 text-xl mt-8 font-lato">
+        {{data.primary.text[0].text}}
+        <DefaultButton @click="goToProjectsPage" class="mt-12">Découvrir mes projets</DefaultButton>
+      </div>
     </div>
     <div class="w-1/3 h-[568px]" v-if="data.primary">
       <img class="object-cover object-center w-full h-full" :src="data.primary.image.url" :alt="data.primary.image.alt">
