14430dc91f8ea615964f89dc0af8a88a3e508cd5
Vincent <vincecreton@hotmail.fr>
2023-08-18 15:43:10 +0200

    WIP: Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { client } = usePrismic()
+const { data: footer } = await useAsyncData('general', () => client.getSingle('footer'))
+</script>
+<template>
+  <footer>
+    <h2>{{footer.data.title[0].text}}</h2>
+  </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import HomeHero from "~/src/components/HomeHero.vue";
 import Presentation from "~/src/components/Presentation.vue";
-import {ref} from "vue";
+import Footer from "~/src/components/Footer.vue";
 
 const { client } = usePrismic()
 const { data: home } = await useAsyncData('general', () => client.getSingle('home'))
@@ -18,5 +18,6 @@ const presentationBlock = computed(() => {
   <div class="relative bg-black">
     <HomeHero />
     <Presentation :data="presentationBlock" />
+    <Footer />
   </div>
 </template>
