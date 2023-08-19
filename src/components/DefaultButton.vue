498bb1d460e5f6afd2a367b45737b991873fa2d4
Vincent <vincecreton@hotmail.fr>
2023-08-19 16:23:13 +0200

    FEATURE: V1 of Default button hover effect

--- src/components/DefaultButton.vue
+++ src/components/DefaultButton.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+const props = defineProps({
+  mousePosition: {
+    required: true,
+  },
+  container: {
+    required: true,
+  }
+})
+const xTranslate = computed(() => {
+  return (((props.mousePosition.x - props.container.offsetLeft) - props.container.offsetWidth / 2) / props.container.offsetWidth * 20)
+})
+const yTranslate = computed(() => {
+  return (((props.mousePosition.y - props.container.offsetTop) - props.container.offsetHeight / 2) / props.container.offsetHeight * 20)
+})
+</script>
 <template>
-  <button class="block px-4 py-2 border border-white rounded-full text-sm font-porpora bg-white-secondary/[.05]">
+  <button
+      v-if="mousePosition && container"
+      class="relative block px-4 py-2 border border-white rounded-full text-sm font-porpora bg-white-secondary/[.05]"
+  >
+    <span class="hover-bg" :style="`transform: translate(${xTranslate}px, ${yTranslate}px)`" />
     <slot />
   </button>
 </template>
+<style scoped>
+  button:hover .hover-bg {
+    opacity: 0.75;
+  }
+  .hover-bg {
+    transition: all 0.5s;
+    position: absolute;
+    opacity: 0;
+    inset: -1px; /* control the spread */
+    z-index: -1; /* place the element behind */
+    background: -webkit-linear-gradient(-180deg, #953621 0%, #FFA723 51.04%, #FFDC5F 100%);
+    filter: blur(15px); /* control the blur */
+  }
+</style>
--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
 import DefaultButton from "~/src/components/DefaultButton.vue";
+
 defineProps<{
   data: object
 }>()
+const mousePosition = ref({x: 0, y:0})
+const container = ref()
 const goToProjectsPage = async () => {
   await navigateTo({ name: 'projects-list' })
 }
+const getMousePosition = (e: MouseEvent) => {
+  mousePosition.value.x = e.pageX
+  mousePosition.value.y = e.pageY
+}
 </script>
 <template>
-  <div class="flex px-10 items-start">
+  <div @mousemove="getMousePosition" class="flex px-10 items-start">
     <div v-if="data.primary" class="w-2/3 flex flex-wrap justify-center text-white">
       <h2 class="w-full text-4xl font-title">{{data.primary.title[0].text}}</h2>
       <div class="w-3/4 text-xl mt-8 font-lato">
         {{data.primary.text[0].text}}
-        <DefaultButton @click="goToProjectsPage" class="mt-12">Découvrir mes projets</DefaultButton>
+        <div class="w-fit" ref="container">
+          <DefaultButton :container="container" :mouse-position="mousePosition" @click="goToProjectsPage" class="mt-12">Découvrir mes projets</DefaultButton>
+        </div>
       </div>
     </div>
     <div class="w-1/3 h-[568px]" v-if="data.primary">
