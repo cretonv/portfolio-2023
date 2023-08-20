12356fcf1b51df5345e622c0fe0efb40bdc752fa
Vincent <vincecreton@hotmail.fr>
2023-08-20 16:17:05 +0200

    FEATURE: Anim hero title apparition V1

--- components/TitleH1.vue
+++ components/TitleH1.vue
@@ -1,6 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {onMounted} from "@vue/runtime-core";
+import SplitType from "split-type";
+import {gsap} from "gsap";
+
+const title = ref()
+let splitTitle
+onMounted(() => {
+  splitTitle = SplitType.create(title.value, {types: 'lines, chars'})
+  let lineCharCounter: number
+  let lineCounter: number = 1
+
+  if(splitTitle.lines) {
+    splitTitle.lines.forEach((line) => {
+      lineCharCounter = 0
+      Array.from(line.children).forEach((char) => {
+        gsap.from(char, {
+          y: 60 + 10 * lineCharCounter,
+          duration: 0.75,
+          stagger: { amount: 1.1 * lineCharCounter * lineCounter },
+          ease: "sine.out"
+        })
+        lineCharCounter ++
+      })
+    })
+    lineCounter ++
+  }
+})
+</script>
 <template>
-  <h1 class="text-white"><slot /></h1>
+  <h1 ref="title" class="text-white"><slot /></h1>
 </template>
 <style>
 h1 {
@@ -8,11 +36,15 @@ h1 {
   font-weight: 300;
   font-size: 91px;
   line-height: 123px;
+  font-kerning: none;
 }
 @media (max-width: 768px) {
     h1 {
         font-size: 40px;
         line-height: 56px;
     }
+}
+.line {
+  overflow: hidden;
 }
 </style>
