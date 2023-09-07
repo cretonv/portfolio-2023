ee60977e79806deca2ca7baef8fe8095371eac86
Vincent <vincecreton@hotmail.fr>
2023-09-07 17:19:58 +0200

    WIP: Prismic slice component integration

---
+++ components/slices/SliderImage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {defineProps} from 'vue'
+const props = defineProps<{
+  slice: Object
+}>()
+</script>
+<template>
+  <div class="flex">
+    Ici j'aurais mon composant slider image.
+  </div>
+</template>
