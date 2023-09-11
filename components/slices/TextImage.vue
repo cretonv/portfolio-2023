1c5d6f5d1c7c5ccd89c052a3b04a74074d035644
Vincent <vincecreton@hotmail.fr>
2023-09-11 16:47:32 +0200

    FEATURE: Slice image text v1

--- components/slices/TextImage.vue
+++ components/slices/TextImage.vue
@@ -5,7 +5,15 @@ const props = defineProps<{
 }>()
 </script>
 <template>
-  <div class="flex">
-    Ici j'aurais mon composant text + image.
+  <div v-if="slice.primary.text && slice.primary.image" class="flex items-stretch px-10 justify-between">
+    <div class="pl-10 w-7/12 pr-4">
+      <PrismicImage
+          class="object-center w-full"
+          :class="slice.primary.img_contain ? 'object-contain h-auto' : 'object-cover'"
+          :field="slice.primary.image" />
+    </div>
+    <div class="w-4/12 flex items-center">
+      <PrismicRichText class="rich-text w-full py-10 pr-8" :field="slice.primary.text" />
+    </div>
   </div>
 </template>
