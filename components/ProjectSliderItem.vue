23ed02f73b27eb09067492bfea3e2a23610867b6
Vincent <vincecreton@hotmail.fr>
2023-09-13 15:34:51 +0200

    FEATURE: Project slider item component

---
+++ components/ProjectSliderItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {defineProps} from "vue"
+const props = defineProps<{
+  uid: string,
+  client: object
+}>()
+const {data: project } = await useAsyncData(props.uid, () => props.client.getByUID('project', props.uid))
+</script>
+<template>
+  <div class="item-container relative">
+    <PrismicImage class="absolute left-0 right-0 object-cover object-center w-full h-full" :field="project.data.list_thumbnail" />
+    <img class="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :src="project.data.logo.url" :alt="project.data.logo.alt">
+  </div>
+</template>
+<style scoped>
+.item-container::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  opacity: 0.2;
+  z-index: 1;
+}
+</style>
