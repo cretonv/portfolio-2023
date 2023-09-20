2a442e58b986db37b59269658806fb54573be902
Vincent <vincecreton@hotmail.fr>
2023-09-20 09:56:27 +0200

    FEATURE: Refacto images slider with background and contain image

--- components/slices/SliderImage.vue
+++ components/slices/SliderImage.vue
@@ -55,8 +55,13 @@ const initGsap = () => {
 <template>
   <div ref="container" class="overflow-hidden my-24 -md:my-16">
     <div ref="slider" class="flex w-fit pl-20 gap-4 -md:pl-4">
-      <div v-for="slide in slice.items" class="h-56 w-80">
-        <PrismicImage class="object-cover object-center w-full h-56 w-80" :field="slide.image" />
+      <div v-for="slide in slice.items" class="relative h-56 w-80">
+        <PrismicImage
+            v-if="slide.image"
+            class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 object-contain object-center w-full h-full max-w-[80%] max-h-[80%]"
+            :field="slide.image"
+        />
+        <PrismicImage v-if="slide.bg_img" class="object-cover object-center w-full h-56 w-80" :field="slide.bg_img" />
       </div>
     </div>
   </div>
