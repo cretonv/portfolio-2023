cdfebbfefb37f340898210bc4549fb4809e09a6c
Vincent <vincecreton@hotmail.fr>
2023-09-20 10:25:37 +0200

    FEATURE: Refacto images Double Image  with background and contain image

--- components/slices/DoubleImage.vue
+++ components/slices/DoubleImage.vue
@@ -6,8 +6,21 @@ const props = defineProps<{
 </script>
 <template>
   <div class="flex gap-4 pl-20 pr-10 w-full my-24 -md:my-16 -md:px-4 -md:flex-wrap">
-    <div v-for="image in slice.primary" class="h-[436px] w-1/2 -md:w-full -md:h-[264px]">
-      <PrismicImage class="object-cover object-center w-full h-full" :field="image" />
+    <div class="relative h-[436px] w-1/2 -md:w-full -md:h-[264px]">
+      <PrismicImage
+          v-if="slice.primary.left_img.url"
+          class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 object-contain object-center w-full h-full max-w-[80%] max-h-[80%]"
+          :field="slice.primary.left_img"
+      />
+      <PrismicImage v-if="slice.primary.bg_img_left.url" class="object-cover object-center w-full h-full" :field="slice.primary.bg_img_left" />
+    </div>
+    <div class="relative h-[436px] w-1/2 -md:w-full -md:h-[264px]">
+      <PrismicImage
+          v-if="slice.primary.right_img.url"
+          class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 object-contain object-center w-full h-full max-w-[80%] max-h-[80%]"
+          :field="slice.primary.right_img"
+      />
+      <PrismicImage v-if="slice.primary.bg_img_right.url" class="object-cover object-center w-full h-full" :field="slice.primary.bg_img_right" />
     </div>
   </div>
 </template>
