0182fdea9c53daf30a4e6b0a6fa9b5b8d7f58b6b
Vincent <vincecreton@hotmail.fr>
2023-09-20 11:58:41 +0200

    FEATURE: Refacto image with background and contain image

--- components/slices/Image.vue
+++ components/slices/Image.vue
@@ -6,7 +6,15 @@ const props = defineProps<{
 }>()
 </script>
 <template>
-  <div class="pr-10 pl-20 my-24 h-screen overflow-hidden -md:my-16 -md:px-4 -md:h-1/2">
-    <PrismicImage class="object-cover object-center w-full h-full" :field="slice.primary.image" />
+  <div class="relative pr-10 pl-20 my-24 h-screen overflow-hidden -md:my-16 -md:px-4 -md:h-1/2">
+    <PrismicImage
+        v-if="slice.primary.image.url"
+        class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 object-contain object-center w-full h-full max-w-[80%] max-h-[80%]"
+        :field="slice.primary.image" />
+    <PrismicImage
+        v-if="slice.primary.bg_img.url"
+        class="object-cover object-center w-full h-full"
+        :field="slice.primary.bg_img"
+    />
   </div>
 </template>
